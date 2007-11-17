3fa6816d7b26c6e36d2071de82e4ec7af61f6352
Andy Armstrong <andy@hexten.net>
2007-11-17 17:31:13 +0000

    Tagged 0.05, version bump

--- lib/App/PerlPPP.pm
+++ lib/App/PerlPPP.pm
@@ -13,11 +13,11 @@ App::PerlPPP - Command line tool for Perfect Paper Passwords
 
 =head1 VERSION
 
-This document describes App::PerlPPP version 0.04
+This document describes App::PerlPPP version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
@@ -64,12 +64,15 @@ our $VERSION = '0.04';
         }
 
         %ARG_SPEC = (
-            show_man  => [0],
-            show_help => [0],
-            args      => _array_spec(),
-            columns   => _range_spec( 'columns', 7, 1, 100 ),
-            rows      => _range_spec( 'rows', 10, 1, 100 ),
-            key       => [
+            show_man   => [0],
+            show_help  => [0],
+            args       => _array_spec(),
+            columns    => _range_spec( 'columns', 7, 1, 100 ),
+            rows       => _range_spec( 'rows', 10, 1, 100 ),
+            codelen    => [undef],
+            passphrase => [undef],
+            alphabet   => [undef],
+            key        => [
                 undef,
                 sub {
                     my ( $self, $key ) = @_;
@@ -88,14 +91,12 @@ our $VERSION = '0.04';
             my $validator = $spec->[1] || sub { shift; shift };
             *{ __PACKAGE__ . '::' . $name } = sub {
                 my $self = shift;
-                unless ( @_ ) {
-                    my $value = $self->{$name};
-                    return ( wantarray && 'ARRAY' eq ref $value )
-                      ? @$value
-                      : $value;
-                }
-                $self->{$name} = $self->$validator( @_ );
-                return;
+                $self->{$name} = $self->$validator( @_ )
+                  if ( @_ );
+                my $value = $self->{$name};
+                return ( wantarray && 'ARRAY' eq ref $value )
+                  ? @$value
+                  : $value;
             };
         }
     }
@@ -121,11 +122,17 @@ our $VERSION = '0.04';
 
 =head2 C<< args >>
 
+=head2 C<< alphabet >>
+
+=head2 C<< codelen >>
+
 =head2 C<< columns >>
 
 =head2 C<< rows >>
 
 =head2 C<< key >>
+
+=head2 C<< passphrase >>
 
 =head2 C<< title >>
 
@@ -145,12 +152,15 @@ sub parse_args {
     my %options;
 
     GetOptions(
-        'help|?'    => \$options{show_help},
-        man         => \$options{show_man},
-        'key=s'     => \$options{key},
-        'columns=i' => \$options{columns},
-        'rows=i'    => \$options{rows},
-        'title=s'   => \$options{title},
+        'help|?'       => \$options{show_help},
+        man            => \$options{show_man},
+        'key=s'        => \$options{key},
+        'passphrase=s' => \$options{passphrase},
+        'columns=i'    => \$options{columns},
+        'rows=i'       => \$options{rows},
+        'title=s'      => \$options{title},
+        'alphabet=s'   => \$options{alphabet},
+        'codelen=i'    => \$options{codelen},
     ) or pod2usage();
 
     while ( my ( $name, $value ) = each %options ) {
@@ -167,11 +177,8 @@ sub parse_args {
 sub run {
     my $self = shift;
 
-    use Data::Dumper;
-    print Dumper( $self );
-
     if ( $self->show_help ) {
-        pod2usage( -verbose => 1 );
+        $self->do_help;
     }
     elsif ( $self->show_man ) {
         pod2usage( -verbose => 2, -exitstatus => 0 );
@@ -203,22 +210,96 @@ sub do_card {
     die "Card numbers start at 1\n" if $card_no < 1;
 
     my $title = $self->title;
-    print "$title [$card_no]\n";
-    my $rows = $self->rows;
-    my $cols = $self->columns;
-    my $ppp  = $self->_make_ppp;
+    my $rows  = $self->rows;
+    my $cols  = $self->columns;
+    my $ppp   = $self->_make_ppp;
     my @passcodes
-      = $ppp->passcodes( $card_no, $rows * $cols, $self->key );
+      = $ppp->passcodes( $card_no, $rows * $cols, $self->_get_key );
 
-    my $colw = length( $passcodes[0] );
-    
-    
-    print join( ", ", @passcodes ), "\n";
+    my $colw   = length( $passcodes[0] );
+    my $center = sub {
+        my $str = shift;
+        my $pad = $colw - length $str;
+        return $str if $pad <= 0;
+        return ( ' ' x ( $pad / 2 ) ) . $str
+          . ( ' ' x ( ( $pad + 1 ) / 2 ) );
+    };
+
+    my $row_fmt = "%4d";
+    my @hdr     = ( ' ' x ( length sprintf( $row_fmt, 1 ) ) );
+    my $col     = 'A';
+    push @hdr, $center->( $col++ ) for ( 1 .. $cols );
+    my $hdr = join( ' ', @hdr );
+    my $rule = '=' x length $hdr;
+    print "$title [$card_no]\n$rule\n$hdr\n$rule\n";
+    for ( 1 .. $rows ) {
+        print join( ' ',
+            sprintf( $row_fmt, $_ ),
+            splice( @passcodes, 0, $cols ) ),
+          "\n";
+    }
+    print "$rule\n";
+}
+
+=head2 C<do_newkey>
+
+Create and display a new random key.
+
+=cut
+
+sub do_newkey {
+    my $self = shift;
+    if ( my $key = $self->_make_key ) {
+        print "Specified key is $key\n";
+    }
+    else {
+        print "Generated key is ",
+          $self->key( $self->_make_ppp->random_sequence ), "\n";
+    }
+}
+
+=head2 C<do_help>
+
+Output help page
+
+=cut
+
+sub do_help {
+    my $self = shift;
+    pod2usage( -verbose => 1 );
+}
+
+sub _make_key {
+    my $self = shift;
+
+    if ( defined( my $key = $self->key ) ) {
+        return $key;
+    }
+    elsif ( defined( my $phrase = $self->passphrase ) ) {
+        return $self->key(
+            $self->_make_ppp->sequence_from_key( $phrase ) );
+    }
+    else {
+        return;
+    }
+}
+
+sub _get_key {
+    my $self = shift;
+    return $self->_make_key
+      || die "Must supply --key or --passphrase\n";
 }
 
 sub _make_ppp {
     my $self = shift;
-    return Crypt::PerfectPaperPasswords->new;
+    my %args;
+    for my $a ( qw( alphabet codelen ) ) {
+        if ( defined( my $value = $self->$a() ) ) {
+            $args{$a} = $value;
+        }
+    }
+
+    return $self->{_ppp} ||= Crypt::PerfectPaperPasswords->new( %args );
 }
 
 1;
